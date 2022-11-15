b37dc1e3f7c3e05b16f864a8591d855b1204c8b2
M-vanDijk <94120142+M-vanDijk@users.noreply.github.com>
2022-11-15 12:55:05 +0100

    update nav
    added basic navigation.

--- templates/defaults/header.php
+++ templates/defaults/header.php
@@ -1,5 +1,5 @@
 <header>
-    <div class="p-3 profilePicture">
+    <div class="p-3">
         <img src="img/dikke_jan.jpg" class="profilePicture rounded-circle img-fluid" id="profilePicture" alt="dikke jan">
     </div>
 </header>
--- templates/defaults/nav.php
+++ templates/defaults/nav.php
@@ -1,1 +1,20 @@
-<?php
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid d-flex">
+        <div class="d-flex flex-grow-1 justify-content-center" id="">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
