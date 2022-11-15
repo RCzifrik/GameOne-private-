10dc72539cdac6898b8e7c4abd626d2eca386d0e
RCzifrik <94119911+RCzifrik@users.noreply.github.com>
2022-11-15 12:01:01 +0100

    head en header php pagina's

--- templates/defaults/header.php
+++ templates/defaults/header.php
@@ -1,1 +1,5 @@
-<?php
+<header>
+    <div class="p-3 profilePicture">
+        <img src="img/dikke_jan.jpg" class="profilePicture rounded-circle img-fluid" id="profilePicture" alt="dikke jan">
+    </div>
+</header>
