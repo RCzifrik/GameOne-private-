c059743576b6df1f4cd5e5d0503fc731f03546ee
zaiyrieyu <92578823+Zeenath-Soekha@users.noreply.github.com>
2022-11-16 23:45:40 +0100

    added nav

--- templates/defaults/nav.php
+++ templates/defaults/nav.php
@@ -1,1 +1,10 @@
-<?php
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid d-flex justify-content-center">
+    <div class="navbar-nav">
+      <a class="nav-link active" aria-current="page" href="#">Home</a>
+      <a class="nav-link" href="#">Features</a>
+      <a class="nav-link" href="#">Pricing</a>
+    </div>
+  </div>
+</nav>
+
