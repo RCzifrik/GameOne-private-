d98311f17b567aeba2fbb3fcf32fa3a897da3a2d
M-vanDijk <94120142+M-vanDijk@users.noreply.github.com>
2022-11-22 10:05:33 +0100

    update
    fixed a few issues

--- templates/defaults/homeMain.php
+++ templates/defaults/homeMain.php
@@ -1,43 +1,38 @@
-<main>
-    <?php
-    include_once ('nav.php');
-    ?>
-    <div class="container-md">
-        <row>
-            <div class="col d-flex justify-content-center pt-5">
-                <div class="card d-inline-flex mx-5" style="width:25%">
-                    <img src="./img/placeholder.png">
-                    <div class="card-img-overlay text-center">
-                        <h5 class="card-title text-light text-center">Card title</h5>
-                        <a href="#" class="btn btn-dark cardButton">Go to collection</a>
-                    </div>
-                </div>
-                <div class="card d-inline-flex mx-5" style="width:25%">
-                    <img src="./img/placeholder.png">
-                    <div class="card-img-overlay text-center">
-                        <h5 class="card-title text-light text-center">Card title</h5>
-                        <a href="#" class="btn btn-dark cardButton">Go to collection</a>
-                    </div>
+<div class="container-md">
+    <row>
+        <div class="col d-flex justify-content-center pt-5">
+            <div class="card d-inline-flex mx-5" style="width:25%">
+                <img src="./img/placeholder.png">
+                <div class="card-img-overlay text-center">
+                    <h5 class="card-title text-light text-center">Card title</h5>
+                    <a href="#" class="btn btn-dark cardButton">Go to collection</a>
                 </div>
             </div>
-        </row>
-        <row>
-            <div class="col d-flex justify-content-center pt-5">
-                <div class="card d-inline-flex mx-5" style="width:25%">
-                    <img src="./img/placeholder.png">
-                    <div class="card-img-overlay text-center">
-                        <h5 class="card-title text-light text-center">Card title</h5>
-                        <a href="#" class="btn btn-dark cardButton">Go to collection</a>
-                    </div>
-                </div>
-                <div class="card d-inline-flex mx-5" style="width:25%">
-                    <img src="./img/placeholder.png">
-                    <div class="card-img-overlay text-center">
-                        <h5 class="card-title text-light text-center">Card title</h5>
-                        <a href="#" class="btn btn-dark cardButton">Go to collection</a>
-                    </div>
+            <div class="card d-inline-flex mx-5" style="width:25%">
+                <img src="./img/placeholder.png">
+                <div class="card-img-overlay text-center">
+                    <h5 class="card-title text-light text-center">Card title</h5>
+                    <a href="#" class="btn btn-dark cardButton">Go to collection</a>
                 </div>
             </div>
-        </row>
-    </div>
-</main>
+        </div>
+    </row>
+    <row>
+        <div class="col d-flex justify-content-center pt-5">
+            <div class="card d-inline-flex mx-5" style="width:25%">
+                <img src="./img/placeholder.png">
+                <div class="card-img-overlay text-center">
+                    <h5 class="card-title text-light text-center">Card title</h5>
+                    <a href="#" class="btn btn-dark cardButton">Go to collection</a>
+                </div>
+            </div>
+            <div class="card d-inline-flex mx-5" style="width:25%">
+                <img src="./img/placeholder.png">
+                <div class="card-img-overlay text-center">
+                    <h5 class="card-title text-light text-center">Card title</h5>
+                    <a href="#" class="btn btn-dark cardButton">Go to collection</a>
+                </div>
+            </div>
+        </div>
+    </row>
+</div>
--- templates/home.php
+++ templates/home.php
@@ -5,8 +5,13 @@
     <body>
         <?php
             include_once ('defaults/header.php');
-            include_once('defaults/homeMain.php');
-        ?>
+            ?>
+        <main>
+            <?php
+                include_once('defaults/nav.php');
+                include_once('defaults/homeMain.php');
+            ?>
+        </main>
         <!-- JavaScript Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
         <script src="js/main.js" rel="javascript/application" type="text/javascript"></script>
