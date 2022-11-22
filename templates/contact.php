7eef56737d877571fe69188ba43687583bd3813c
RCzifrik <94119911+RCzifrik@users.noreply.github.com>
2022-11-22 12:57:14 +0100

    Contactpagina gemaakt, niet gelinkt

---
+++ templates/contact.php
@@ -0,0 +1,30 @@
+<html>
+<?php include_once ('defaults/head.php'); ?>
+<body>
+<?php include_once ('defaults/header.php'); ?>
+<main>
+    <?php
+        include_once ('defaults/nav.php');
+    ?>
+    <div class="container-md text-light text-center">
+        <h2>Wil je meer weten over ons?</h2>
+        <p>Kom dan langs in éen van onze winkels of bel ons!</p>
+        <h4>Tel: </h4>
+        <p>06 12345678</p>
+        <br>
+        <h4>Email: </h4>
+        <p>GameOne@email.nl</p>
+        <br><br>
+        <h3>Onze winkel locatie: </h3>
+        <p>Oude Langendijk 11, 2611 GK Delft</p>
+        <br>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d613.9426995124245!2d4.358520582642736!3d52.011054327895565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5b5c2386bcc91%3A0x808e4d87527325cc!2sGame%20Mania!5e0!3m2!1snl!2snl!4v1669117481436!5m2!1snl!2snl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    </div>
+</main>
+<?php include_once ('defaults/footer.php'); ?>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script src="js/main.js" rel="javascript/application" type="text/javascript"></script>
+</body>
+</html>
