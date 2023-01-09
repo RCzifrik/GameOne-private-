16bd33387e25c9a0a014158031e658ba73c7f415
RCzifrik <94119911+RCzifrik@users.noreply.github.com>
2023-01-09 13:19:44 +0100

    Update gameone.sql

--- database/gameone.sql
+++ database/gameone.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 14 dec 2022 om 12:22
--- Serverversie: 10.4.24-MariaDB
--- PHP-versie: 8.1.6
+-- Generation Time: Jan 09, 2023 at 01:14 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,99 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `game`
---
-
-CREATE TABLE `game` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` varchar(300) NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `genre_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Gegevens worden geëxporteerd voor tabel `game`
---
-
-INSERT INTO `game` (`id`, `name`, `description`, `image`, `genre_id`) VALUES
-(1, 'Elden Ring', 'Torture', 'img/ELDEN_RING.jpg', 3),
-(2, 'Need for Speed', 'Cancer.', 'img/NFS.jpg', 2),
-(3, 'Dying Light', 'Zombie go die', 'img/DYING_LIGHT.jpg', 4),
-(4, 'Mario Kart', 'bad kart racer', 'img/MARIO_KART.jpg', 2),
-(5, 'Crash Team Racing', 'Good kart racer.', 'img/CTR.jpg', 2),
-(6, 'Super Mario Bros', 'old platformer', 'img/SUPER_MARIO_BROS.jpg', 1);
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `genre`
---
-
-CREATE TABLE `genre` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `image` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Gegevens worden geëxporteerd voor tabel `genre`
---
-
-INSERT INTO `genre` (`id`, `name`, `image`) VALUES
-(1, 'Platformer', 'img/GENRE_PLATFORMER.jpg'),
-(2, 'Racing', 'img/GENRE_RACING.jpg'),
-(3, 'Open world', 'img/GENRE_OPEN_WORLD.jpg'),
-(4, 'Fighting', 'img/GENRE_FIGHTING.jpg'),
-(5, 'RPG', 'img/GENRE_RPG.jpg'),
-(6, 'Action', 'img/GENRE_ACTION.jpg');
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `review`
---
-
-CREATE TABLE `review` (
-  `id` int(11) NOT NULL,
-  `score` int(2) NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `description` varchar(300) NOT NULL,
-  `game_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Gegevens worden geëxporteerd voor tabel `review`
---
-
-INSERT INTO `review` (`id`, `score`, `title`, `description`, `game_id`, `user_id`) VALUES
-(1, 10, 'BESTE GAME EVERR8GERGER', 'Zo\'n goed spel, mijn playstation pleegde zelfdood', 5, 4);
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `role`
---
-
-CREATE TABLE `role` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Gegevens worden geëxporteerd voor tabel `role`
---
-
-INSERT INTO `role` (`id`, `name`) VALUES
-(1, 'Admin'),
-(2, 'User'),
-(3, 'Guest');
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -126,111 +34,36 @@ CREATE TABLE `user` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `profile_picture` varchar(255) DEFAULT NULL,
-  `role_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Gegevens worden geëxporteerd voor tabel `user`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `first_name`, `last_name`, `username`, `password`, `profile_picture`, `role_id`) VALUES
-(3, 'Richard', 'Czifrik', 'Mashy Potato', '$2y$10$kBe/QYpl3sU62ejLcdOmN.txRZtwwIywjRpWttQE1sPZUfYs1C7F.', 'img/MASHY_POTATO.jpg', 1),
-(4, 'Stijn', 'Veldhuis', 'Stijntje2010', 'wachtwoord123', 'img/STIJNTJE123.jpg', 2);
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `username`, `password`, `profile_picture`, `role`) VALUES
+(3, 'Richard', 'Czifrik', 'Mashy Potato', '$2y$10$kBe/QYpl3sU62ejLcdOmN.txRZtwwIywjRpWttQE1sPZUfYs1C7F.', 'img/MASHY_POTATO.jpg', 'Admin'),
+(4, 'Stijn', 'Veldhuis', 'Stijntje2010', 'wachtwoord123', 'img/STIJNTJE123.jpg', 'Member');
 
 --
--- Indexen voor geëxporteerde tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indexen voor tabel `game`
---
-ALTER TABLE `game`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `genre_id` (`genre_id`);
-
---
--- Indexen voor tabel `genre`
---
-ALTER TABLE `genre`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexen voor tabel `review`
---
-ALTER TABLE `review`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `review_game_fk` (`game_id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexen voor tabel `role`
---
-ALTER TABLE `role`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexen voor tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `role_id` (`role_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT voor geëxporteerde tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT voor een tabel `game`
---
-ALTER TABLE `game`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT voor een tabel `genre`
---
-ALTER TABLE `genre`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT voor een tabel `review`
---
-ALTER TABLE `review`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT voor een tabel `role`
---
-ALTER TABLE `role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT voor een tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- Beperkingen voor geëxporteerde tabellen
---
-
---
--- Beperkingen voor tabel `game`
---
-ALTER TABLE `game`
-  ADD CONSTRAINT `game_ibfk_1` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`);
-
---
--- Beperkingen voor tabel `review`
---
-ALTER TABLE `review`
-  ADD CONSTRAINT `review_game_fk` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`),
-  ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-
---
--- Beperkingen voor tabel `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
