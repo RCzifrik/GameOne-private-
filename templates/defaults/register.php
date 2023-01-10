a5fc1648be119c15f2b6fc60844a852726518039
Zeenath-Soekha <302458451@student.rocmondriaan.nl>
2023-01-10 09:34:58 +0100

    added register page, css and component

---
+++ templates/defaults/register.php
@@ -0,0 +1,12 @@
+<div class="RaMRwPna container-fluid d-flex justify-content-center flex-grow-1 align-items-center flex-column">
+	<form method="POST" action="" class="d-flex flex-column gap-3 p-5 mb-0 rounded">
+		<input type="text" name="first_name" id="first-name" placeholder="First name" class="ab">
+		<input type="text" name="last_name" id="last-name" placeholder="Last name" class="ab">
+		<input type="text" name="username" id="username" placeholder="Username" class="ab">
+		<input type="password" name="password" id="password" placeholder="Password" class="ab">
+		<div class="d-flex flex-column gap-1">
+			<a href="#" class="fBLpnJUC">Forgot password?</a>
+			<button type="submit" class="ad btn btn-dark">Register</button>
+		</div>
+	</form>
+</div>
---
+++ templates/register.php
@@ -0,0 +1,13 @@
+<?php
+	include_once ('./defaults/head.php'); 
+?>
+<html>
+	<body>
+		<?php 
+			include_once('./defaults/header.php'); 
+			include_once('./defaults/nav.php');  
+			include_once('./defaults/register.php');  
+			include_once('./defaults/footer.php');
+		?>
+	</body>
+</html>
